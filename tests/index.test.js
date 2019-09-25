689286a56631eb2175bcfb83426a77879abce01b
Jawad Stouli <jawad.stouli@gmail.com>
2019-09-25 07:31:51 -0700

    Test invalid JSON message

--- tests/index.test.js
+++ tests/index.test.js
@@ -20,6 +20,9 @@ describe('DidomiSDK', () => {
     expect(sdkScript).toExist();
     expect(sdkScript.src).toEqual('https://sdk.privacy-center.org/03f1af55-a479-4c1f-891a-7481345171ce/loader.js?target=localhost');
 
+    // Ensuring that no error is thrown when a message with invalid JSON is sent to the window
+    window.postMessage('test', '*');
+
     return new Promise((resolve) => {
       window.didomiOnReady = window.didomiOnReady || [];
       window.didomiOnReady.push(resolve);
